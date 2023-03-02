460416016d3d81593e3bdb1ae98b50787279054f
Angelo Luisi <angeloom@ciandt.com>
2023-03-02 11:43:58 -0300

    Hover Concluido

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -6,27 +6,45 @@ import styles from './sidebar.module.scss'
 export const diamondAsset = createIcon(diamond)
 
 export function Sidebar() {
+  function handleClick() {
+    console.log('Teste')
+  }
   return (
     <Box bg="neutral.900" width="12.5rem" height="100%" minHeight="43.5rem">
       <div className={styles.juditeIcon}>
         <img src={juditeIcon.src} />
       </div>
       <UnorderedList>
-        <ListItem color="gray.200" listStyleType="none" key="whatever-key">
+        <ListItem
+          color="gray.200"
+          listStyleType="none"
+          key="whatever-key"
+          onClick={handleClick}
+        >
           {/* <ListIcon as={diamondAsset} /> */}
           <div className={styles.items}>
             <img src={diamond.src} alt="Diamond" height="22" width="22" />
             <h6 className={styles.itemsText}>Enabled</h6>
           </div>
         </ListItem>
-        <ListItem color="gray.200" listStyleType="none" key="whatever-key">
+        <ListItem
+          color="gray.200"
+          listStyleType="none"
+          key="whatever-key"
+          onClick={handleClick}
+        >
           {/* <ListIcon as={diamondAsset} /> */}
           <div className={styles.items}>
             <img src={diamond.src} alt="Diamond" height="22" width="22" />
             <h6 className={styles.itemsText}>Disabled</h6>
           </div>
         </ListItem>
-        <ListItem color="gray.200" listStyleType="none" key="whatever-key">
+        <ListItem
+          color="gray.200"
+          listStyleType="none"
+          key="whatever-key"
+          onClick={handleClick}
+        >
           {/* <ListIcon as={diamondAsset} /> */}
           <div className={styles.items}>
             <img src={diamond.src} alt="Diamond" height="22" width="22" />
